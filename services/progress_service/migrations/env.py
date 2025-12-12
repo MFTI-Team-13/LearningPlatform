f006906c1c9ae2e27285a31314d72809e903fc4c
Neto test <fengshui08@yandex.ru>
2025-12-13 02:03:43 +0300

    feat: added the initial framework

---
+++ services/progress_service/migrations/env.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+import asyncio
+import os
+from logging.config import fileConfig
+
+from alembic import context
+from sqlalchemy import pool
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+
+from app.common.db import models_registry  # noqa: F401
+from app.common.db.base import Base
+from app.core.config import DATABASE_URL
+
+config = context.config
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+config.set_main_option("sqlalchemy.url", str(DATABASE_URL))
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline() -> None:
+    context.configure(
+        url=DATABASE_URL,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def do_run_migrations(connection: Connection) -> None:
+    context.configure(connection=connection, target_metadata=target_metadata)
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    connectable: AsyncEngine = create_async_engine(
+        DATABASE_URL, poolclass=pool.NullPool
+    )
+
+    async def _run() -> None:
+        async with connectable.connect() as async_conn:
+            await async_conn.run_sync(do_run_migrations)
+        await connectable.dispose()
+
+    asyncio.run(_run())
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
